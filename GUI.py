fa3226bf108b2094c07815b9183d19fe515f85dd
trackrunner78 <seanny7788@gmail.com>
2017-11-29 16:33:40 -0600

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,44 @@
+from tkinter  import *
+from PIL import Image, ImageTk
+
+
+class GUI(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+        self.pack(fill=BOTH, expand=1)
+
+    def init_window(self):
+        simButton = Button(self, text="Similar Scan", height=5, width=10)
+        simButton.place(x=0, y=110)
+
+        dupButton = Button(self, text="Duplicates", height=5, width=10)
+        dupButton.place(x=0, y=195)
+
+        listButton = Button(self, text="Whitelist", height=5, width=10)
+        listButton.place(x=0, y=280)
+
+        cusButton = Button(self, text="Custom Scans", height=5, width=10)
+        cusButton.place(x=0, y=365)
+
+        setButton = Button(self, text="Settings", height=3, width=10)
+        setButton.place(x=0, y=450)
+
+        scanButton = Button(self, text="Scan", bg="Blue", fg="white", height=4, width=20)
+        scanButton.place(x=500, y=350)
+
+        dupLabel = Label(self, text="Duplicates:", font=("Sans", 40))
+        dupLabel.place(x=100, y=250)
+
+        load = Image.open
+
+
+
+gui = Tk()
+object = GUI(gui)
+gui.title("Duplicate Detection and Prevention System")
+gui.geometry("800x500")
+gui.resizable(False, False)
+gui.mainloop()
